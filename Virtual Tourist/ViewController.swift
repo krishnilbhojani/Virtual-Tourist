51860881f13d88705deb08a8a922a0114ee730e8
Krishnil Bhojani <krishnilbhojani2001@gmail.com>
2020-05-27 12:26:40 +0530

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Krishnil Bhojani on 27/05/20.
+//  Copyright © 2020 Krishnil Bhojani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
